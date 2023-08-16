d0ab46af80d7dc904025ea08c66b65a9ab23420c
Sabari7373 <sabariganesh7373@gmail.com>
2023-08-16 20:40:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import './App.css';
+import Navbar from './Components/Navbar';
+import HomeScreen from './Screens/HomeScreen';
+import RegisterScreen from './Screens/RegisterScreen';
+import ProductScreen from './Screens/ProductScreen';
+import UserScreen from './Screens/UserScreen';
+import ProductInserScreen from './Screens/ProductInserScreen';
+import LoginScreen from './Screens/LoginScreen';
+
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Navbar/>
+              <h1 style={{color:"white", backgroundColor:"blue", borderRadius:"20px", margin:"100px", marginLeft:"300px"}}>
+                Join the class on time!</h1>
+      
+             
+      <Router>
+      <Routes>
+        <Route path='/home' exact element={<HomeScreen/>}/>
+        <Route path='/register' exact element={<RegisterScreen/>}/>
+        <Route path='/login' exact element={<LoginScreen/>}/>
+        <Route path='/user' exact element={<UserScreen/>}/>
+        <Route path='/product' exact element={<ProductScreen/>}/>
+        <Route path='/productinsert' exact element={<ProductInserScreen/>}/>
+
+      </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
+
+
---
+++ src/Screens/HomeScreen.js
@@ -0,0 +1,22 @@
+import React from 'react'
+function HomeScreen() {
+  return (
+    <div>
+      
+    <div>HomeScreen</div>
+     
+
+    <div className="home">
+        <div>
+          <h1 className="vp">"A book is a dream that you hold in your hand" </h1>
+          <img width={700}
+          className="bookImage"
+          src="https://www.wikihow.com/images/thumb/a/a3/What-book-should-i-read-quiz-image.png/1200px-What-book-should-i-read-quiz-image.png"/>
+        </div>
+      </div>
+    </div>
+
+  )
+}
+
+export default HomeScreen
